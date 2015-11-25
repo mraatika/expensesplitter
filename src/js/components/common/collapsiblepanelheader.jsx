47da7351eb6ed3924303f0a34062b6e2f852533d
mraatika <raatikaine@gmail.com>
2015-11-25 13:21:37 +0200

    If onExpand is omitted then don't render the toggle button

--- src/js/components/common/collapsiblepanelheader.jsx
+++ src/js/components/common/collapsiblepanelheader.jsx
@@ -16,10 +16,18 @@ export default class CollapsiblePanelHeader extends React.Component {
      * @return {ReactComponent}
      */
     render() {
+        const onExpand = this.props.onExpand;
+        let toggleButton = '';
+
+        // only if onExpand callback is defined is the toggle button relevant
+        if (onExpand) {
+            toggleButton = <i className={'u-pull-right fa fa-' + (this.props.isExpanded ? 'minus' : 'plus') + '-square-o'}/>;
+        }
+
         return (
-            <div className="wrap-content" onClick={this.props.onExpand}>
+            <div className="wrap-content" onClick={onExpand || function() {}}>
                 <span className="u-pull-left bold">{this.props.headerText}</span>
-                <i className={'u-pull-right fa fa-' + (this.props.isExpanded ? 'minus' : 'plus') + '-square-o'}></i>
+                {toggleButton}
             </div>
         );
     }
@@ -31,7 +39,8 @@ export default class CollapsiblePanelHeader extends React.Component {
  */
 CollapsiblePanelHeader.propTypes = {
     /**
-     * Callback function for the collapse toggle button
+     * Callback function for the collapse toggle button.
+     * If omitted the component doesn't have the toggle button.
      * @type {function}
      */
     onExpand: React.PropTypes.func,
