36812b8fbedfe89f78264af481641b5032520afd
mraatika <raatikaine@gmail.com>
2015-11-26 22:12:24 +0200

    Reload after image task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,9 +1,11 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 var config = require('../config').images;
+var connect = require('gulp-connect');
 
 gulp.task('images', () => {
     return gulp.src(config.src)
         .pipe(imagemin(config.settings))
-        .pipe(gulp.dest(config.dest));
+        .pipe(gulp.dest(config.dest))
+        .pipe(connect.reload());
 });
