fc10762eae07f9184249351635440c0a4bdffac6
mraatika <raatikaine@gmail.com>
2016-01-13 14:43:13 +0200

    Removed only call

--- test/components/participants/participant.spec.jsx
+++ test/components/participants/participant.spec.jsx
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import sinon from 'sinon';
 
-describe.only('Component:Participant', function() {
+describe('Component:Participant', function() {
     const jsdom = require('mocha-jsdom');
 
     let React;
