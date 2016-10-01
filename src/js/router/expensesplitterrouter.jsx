227dc60a1efb4a488048c0d65809968b0b7bd068
Miitri Raatikainen <raatikaine@gmail.com>
2016-10-01 11:54:21 +0300

    Nested sheet routes

--- src/js/router/expensesplitterrouter.jsx
+++ src/js/router/expensesplitterrouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
 import App from '../components/app.jsx';
 import HomePage from '../components/home/homepage.jsx';
 import ParticipantsPage from '../components/participants/participantspage.jsx';
@@ -8,6 +7,11 @@ import ExpensesPage from '../components/expenses/expensespage.jsx';
 import TransactionsPage from '../components/transactions/transactionspage.jsx';
 import SummaryPage from '../components/summary/summarypage.jsx';
 
+/**
+ * @class ExpenseSplitterRouter
+ * @description Wrapper component for React Router
+ * @extends {React.Component}
+ */
 export default class ExpenseSplitterRouter extends React.Component {
 
     render() {
@@ -16,11 +20,13 @@ export default class ExpenseSplitterRouter extends React.Component {
                 <Route path="/" component={App}>
                     <IndexRoute component={HomePage} />
 
-                    <Route path="/sheet/:sheetId" component={HomePage} />
-                    <Route path="/sheet/:sheetId/participants" component={ParticipantsPage} />
-                    <Route path="/sheet/:sheetId/expenses" component={ExpensesPage} />
-                    <Route path="/sheet/:sheetId/transactions" component={TransactionsPage} />
-                    <Route path="/sheet/:sheetId/summary" component={SummaryPage} />
+                    <Route path="/sheet/:sheetId">
+                        <IndexRoute component={HomePage} />
+                        <Route path="participants" component={ParticipantsPage} />
+                        <Route path="expenses" component={ExpensesPage} />
+                        <Route path="transactions" component={TransactionsPage} />
+                        <Route path="summary" component={SummaryPage} />
+                    </Route>
                 </Route>
             </Router>
         );
