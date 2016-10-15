85df10d6f1c2f6be8dc60685f1c4f401b91a10cd
Miitri Raatikainen <raatikaine@gmail.com>
2016-10-15 14:02:02 +0300

    Removed gulp directory

--- gulp/tasks/build.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var config = require('../config').watch;
-
-gulp.task('build', ['browserify', 'styles', 'html', 'fonts', 'images'], function() {
-    gulp.src(config.src).pipe(connect.reload());
-});
--- gulp/tasks/html.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var config = require('../config').html;
-
-gulp.task('html', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest));
-});
